b6fb6a92d51e86a499d10c7f8f99a3072284002f
Caique Novaes <caiquenovaes1994@gmail.com>
2026-07-20 15:18:29 -0300

    fix: ajusta politica de cookies (sameSite) para producao

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -39,7 +39,7 @@ exports.login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       maxAge: 24 * 60 * 60 * 1000 // 1 dia
     });
 
@@ -63,6 +63,10 @@ exports.me = (req, res) => {
 };
 
 exports.logout = (req, res) => {
-  res.clearCookie('token');
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+  });
   res.json({ message: 'Logout efetuado com sucesso' });
 };
